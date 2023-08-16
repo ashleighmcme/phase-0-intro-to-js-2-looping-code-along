95c792009994a4aaf8d1b841d1912384515a056f
Ashleigh McMahon <ashleighmc@me.com>
2023-08-16 17:05:47 -0600

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// const thankyou=["Guadalupe", "Ollie", "Aki"]
+function writeCards(array, event){
+    let thankyou = []
+    for (let i=0;i<array.length;i++){
+        thankyou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return thankyou
+}
+
+const count = (10)
+function countDown (count){
+    let countdown = 10;
+    while (countdown >= 0){
+        console.log (countdown --)
+    }
+}
+
+
+
